fa0a3dab5443a62beb9c4e49982e33227d9c25bc
Pham Tan Dung <phamtandung@thaco.com.vn>
2023-05-31 16:07:08 +0700

    update

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,8 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
+
 function ColorBox(props) {
     const { color } = props;
     return (
         <div className="box" style={{ backgroundColor: color }}></div>
     )
 }
+
+ColorBox.propTypes = {
+    color: PropTypes.string.isRequired,
+    rounded: PropTypes.bool,
+};
+ColorBox.defaultProps = {
+    rounded: true
+};
+
 export default ColorBox;
