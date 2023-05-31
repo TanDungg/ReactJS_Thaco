26398622047f4b40e0323a220cca930fdd28a68e
Pham Tan Dung <phamtandung@thaco.com.vn>
2023-05-31 14:59:04 +0700

    Update title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Tan Dung
+          Tan Dung || THACO
         </p>
         <a
           className="App-link"
