032da0a03822337c70f3fcd5c32693ad9046232d
Pham Tan Dung <phamtandung@thaco.com.vn>
2023-06-01 14:50:05 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
-import React from 'react';
-import TodoFeatures from './features/Todo'
-import AlbumFeatures from './features/Album'
+import React, { useState } from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Color from './Color';
+import AlbumFeatures from './features/Album';
+import TodoFeatures from './features/Todo';
+
+const Menu = () => {
+  const [activeItem, setActiveItem] = useState('');
+
+  const handleItemClick = (itemName) => {
+    setActiveItem(itemName);
+  };
+
+  return (
+    <nav className="menu">
+      <ul className="menu-list">
+        <li className={activeItem === 'todos' ? 'active' : ''}>
+          <Link to="/todos" onClick={() => handleItemClick('todos')}>
+            Todos
+          </Link>
+        </li>
+        <li className={activeItem === 'albums' ? 'active' : ''}>
+          <Link to="/albums" onClick={() => handleItemClick('albums')}>
+            Albums
+          </Link>
+        </li>
+        <li className={activeItem === 'colors' ? 'active' : ''}>
+          <Link to="/colors" onClick={() => handleItemClick('colors')}>
+            Colors
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 function App() {
   return (
-    <div className='App'>
-      <TodoFeatures />
-      <AlbumFeatures />
-      <Color />
+    <div className="App">
+      <h1>THACO TRƯỜNG HẢI</h1>
+      <Menu />
+      <Routes>
+        <Route path="/todos" element={<TodoFeatures />} />
+        <Route path="/albums" element={<AlbumFeatures />} />
+        <Route path="/colors" element={<Color />} />
+      </Routes>
     </div>
   );
 }
